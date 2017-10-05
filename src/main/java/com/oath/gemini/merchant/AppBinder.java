3be2a46bf14ed56ae34f84aa05ce8c1d935da5e5
Tong Chen <tong@oath.com>
2017-10-04 17:55:33 -0700

    hibernate binding

--- src/main/java/com/oath/gemini/merchant/AppBinder.java
+++ src/main/java/com/oath/gemini/merchant/AppBinder.java
@@ -1,11 +1,20 @@
 package com.oath.gemini.merchant;
 
+import com.oath.gemini.merchant.db.DatabaseService;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.hibernate.SessionFactory;
 
 public class AppBinder extends AbstractBinder {
+    private final SessionFactory sessionFactory;
+
+    public AppBinder(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     protected void configure() {
-        // TODO Auto-generated method stub
+        // singleton binding
+        bind(sessionFactory).to(SessionFactory.class);
+        bind(DatabaseService.class).to(DatabaseService.class);
     }
 }
