3a19c5e4d423cb8074e79ab859097f50074b7fa3
Tong Chen <tong@oath.com>
2017-10-19 14:49:18 -0700

    catch and log exception before rethrow it

--- src/main/java/com/oath/gemini/merchant/shopify/ShopifyOnboardResource.java
+++ src/main/java/com/oath/gemini/merchant/shopify/ShopifyOnboardResource.java
@@ -19,7 +19,6 @@ import com.oath.gemini.merchant.shopify.json.ShopifyTokenRequestData;
 import com.oath.gemini.merchant.shopify.json.Tag;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
@@ -75,7 +74,7 @@ public class ShopifyOnboardResource {
     @GET
     @Path("welcome")
     public Response install(@Context UriInfo info, @QueryParam("hmac") String hmac, @QueryParam("shop") String shop,
-            @QueryParam("timestamp") String ts) throws MalformedURLException, URISyntaxException {
+            @QueryParam("timestamp") String ts) {
         try {
             String counterHmac = ShopifyOauthHelper.generateHMac("shop=" + shop, "timestamp=" + ts);
             if (!hmac.equals(counterHmac)) {
@@ -88,11 +87,16 @@ public class ShopifyOnboardResource {
         /**
          * Redirect to the store owner for the grant of the access scopes
          */
-        String path = info.getAbsolutePath().toString();
-        String redirectUrl = path.substring(0, path.indexOf("shopify")) + "shopify/home";
+        try {
+            String path = info.getAbsolutePath().toString();
+            String redirectUrl = path.substring(0, path.indexOf("shopify")) + "shopify/home";
 
-        URI uri = buildScopeRequestUrl(shop, redirectUrl);
-        return Response.temporaryRedirect(uri).build();
+            URI uri = buildScopeRequestUrl(shop, redirectUrl);
+            return Response.temporaryRedirect(uri).build();
+        } catch (Exception e) {
+            log.error("failed to validate the legitimate of the call", info.getAbsolutePath());
+            return Response.serverError().entity(e.toString()).build();
+        }
     }
 
     /**
@@ -108,7 +112,7 @@ public class ShopifyOnboardResource {
     @Path("home")
     public Response home(@Context HttpServletRequest req, @QueryParam("hmac") String hmac, @QueryParam("shop") String shop,
             @QueryParam("timestamp") String ts, @QueryParam("code") String code, @QueryParam("state") String state,
-            @DefaultValue("") @QueryParam("_refresh") String _refresh, @DefaultValue("") @QueryParam("_mc") String _mc) throws Exception {
+            @DefaultValue("") @QueryParam("_refresh") String _refresh, @DefaultValue("") @QueryParam("_mc") String _mc) {
 
         // Verify the signature of the call
         try {
@@ -126,31 +130,36 @@ public class ShopifyOnboardResource {
             return Response.ok().build();
         }
 
-        // Ask for the access scopes if our app has not been installed yet
-        ShopifyAccessToken tokens = fetchAuthToken(shop, code);
+        try {
+            // Ask for the access scopes if our app has not been installed yet
+            ShopifyAccessToken tokens = fetchAuthToken(shop, code);
 
-        if (tokens == null || StringUtils.isBlank(tokens.getAccessToken())) {
-            // The shopify code may have expired when user clicks the Browser's Back button to re-play an earlier on-boarding
-            log.error("a shopify code '{}' likely has expired", code);
-            return Response.status(Status.BAD_REQUEST).build();
-        }
+            if (tokens == null || StringUtils.isBlank(tokens.getAccessToken())) {
+                // The shopify code may have expired when user clicks the Browser's Back button to re-play an earlier on-boarding
+                log.error("a shopify code '{}' likely has expired", code);
+                return Response.status(Status.BAD_REQUEST).build();
+            }
 
-        // If Shopify's shop account does not exist, we certainly do not have his Yahoo's Refresh Token, and therefore asks him
-        // to go through Yahoo's OAuth flow
-        StoreAcctEntity storeAcct = databaseService.findStoreAcctByAccessToken(tokens.getAccessToken());
+            // If Shopify's shop account does not exist, we certainly do not have his Yahoo's Refresh Token, and therefore asks him
+            // to go through Yahoo's OAuth flow
+            StoreAcctEntity storeAcct = databaseService.findStoreAcctByAccessToken(tokens.getAccessToken());
 
-        if (storeAcct != null) {
-            return setup(shop, storeAcct.getYahooAccessToken(), storeAcct.getStoreAccessToken());
-        } else {
-            // Redirect to Yahoo OAuth2 handler for user's Gemini access. Will will be redirected to here when OAuth2 done
-            String requestAuth = config.getString("y.oauth.auth.request.url");
-            String rd = new URI(req.getScheme(), config.getString("app.host"), "/g/shopify/ews", null).toString();
+            if (storeAcct != null) {
+                return setupOrRepaireIfRequired(shop, storeAcct.getYahooAccessToken(), storeAcct.getStoreAccessToken());
+            } else {
+                // Redirect to Yahoo OAuth2 handler for user's Gemini access. Will will be redirected to here when OAuth2 done
+                String requestAuth = config.getString("y.oauth.auth.request.url");
+                String rd = new URI(req.getScheme(), config.getString("app.host"), "/g/shopify/ews", null).toString();
 
-            rd = ClosableHttpClient.buildQueries(rd, "_mc", tokens.getAccessToken());
-            rd = ClosableHttpClient.buildQueries(rd, "shop", shop);
+                rd = ClosableHttpClient.buildQueries(rd, "_mc", tokens.getAccessToken());
+                rd = ClosableHttpClient.buildQueries(rd, "shop", shop);
 
-            requestAuth = requestAuth.replace("${y.oauth.redirect}", URLEncoder.encode(rd, "UTF-8"));
-            return Response.temporaryRedirect(URI.create(requestAuth)).build();
+                requestAuth = requestAuth.replace("${y.oauth.redirect}", URLEncoder.encode(rd, "UTF-8"));
+                return Response.temporaryRedirect(URI.create(requestAuth)).build();
+            }
+        } catch (Exception e) {
+            log.error("failed to validate the legitimate of the call", req.getRequestURI());
+            return Response.serverError().entity(e.toString()).build();
         }
     }
 
@@ -161,9 +170,9 @@ public class ShopifyOnboardResource {
     @Path("ews")
     public Response approve(@Context HttpServletRequest req, @DefaultValue("") @QueryParam("code") String code,
             @QueryParam("shop") String shop, @DefaultValue("") @QueryParam("_mc") String _mc) {
-        if (StringUtils.isEmpty(code)) {
+        if (StringUtils.isBlank(code)) {
             // TODO: indicate that the user denies our access request
-            return Response.ok().build();
+            return Response.ok("Aborted!").build();
         }
 
         try {
@@ -173,21 +182,21 @@ public class ShopifyOnboardResource {
             // Redirect user to a campaign setup page
             if (tokens != null && tokens.getRefreshToken() != null) {
                 String refreshToken = tokens.getRefreshToken();
-                return setup(shop, refreshToken, _mc);
+                return setupOrRepaireIfRequired(shop, refreshToken, _mc);
             } else {
                 log.error("invalid EWS authorization code, which could have expired");
+                return Response.status(Status.UNAUTHORIZED).entity("failed to retrieve EWS oAuth token").build();
             }
         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.error("failed to validate the legitimate of the call", req.getRequestURI());
+            return Response.serverError().entity(e.toString()).build();
         }
-        return null;
     }
 
     /**
      * Configure a shopper's campaign, product feed if necessary
      */
-    private Response setup(String shop, String gRefreshToken, String shopifyRefreshToken) throws Exception {
+    private Response setupOrRepaireIfRequired(String shop, String gRefreshToken, String shopifyRefreshToken) throws Exception {
 
         // By now, we have both Shopify and Yahoo access tokens. Let's persist this info locally
         ShopifyClientService ps = new ShopifyClientService(shop, shopifyRefreshToken);
@@ -209,7 +218,7 @@ public class ShopifyOnboardResource {
 
             // By now, we have configured a DPA campaign and its product feed on Gemini site. Let's persist this info locally
             storeCmpEntity.setStoreAcctId(storeAcctEntity.getId());
-            registerStoreCampainIfRequired(ps, ews, storeCmpEntity);
+            registerStoreCampaignIfRequired(ps, ews, storeCmpEntity);
         }
 
         // Inject or modify a dot pixel to tracke user's product events
@@ -287,7 +296,7 @@ public class ShopifyOnboardResource {
     /**
      * To register campaign info if we haven't done so; otherwise update an existing entity
      */
-    private void registerStoreCampainIfRequired(ShopifyClientService ps, EWSClientService ews, StoreCampaignEntity cmpEntity)
+    private void registerStoreCampaignIfRequired(ShopifyClientService ps, EWSClientService ews, StoreCampaignEntity cmpEntity)
             throws Exception {
         StoreCampaignEntity storedEntity = databaseService.findStoreCampaignByGeminiCampaignId(cmpEntity.getCampaignId());
 
@@ -305,6 +314,9 @@ public class ShopifyOnboardResource {
                 databaseService.update(storedEntity);
             }
         } else {
+            String decoratedName = cmpEntity.getName() + "-" + cmpEntity.getCampaignId();
+
+            cmpEntity.setName(decoratedName);
             databaseService.save(cmpEntity);
         }
     }
