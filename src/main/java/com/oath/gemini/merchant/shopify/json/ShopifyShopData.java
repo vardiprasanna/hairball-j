36f4eb9cabc9625ba69e29fc4bc1385bd5f41df9
Tong Chen <tong@oath.com>
2017-11-20 12:45:56 -0800

    refactoring

--- src/main/java/com/oath/gemini/merchant/shopify/json/ShopifyShopData.java
+++ src/main/java/com/oath/gemini/merchant/shopify/json/ShopifyShopData.java
@@ -2,11 +2,15 @@ package com.oath.gemini.merchant.shopify.json;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * @author tong 10/1/2017
+ */
 @Getter
 @Setter
 @JsonTypeName("shop")
@@ -16,23 +20,41 @@ import lombok.Setter;
 public class ShopifyShopData {
     private long id; // 690933842
     private String name; // "Apple Computers"
-    private String shop_owner; // "Steve Jobs"
+
+    @JsonProperty("shop_owner")
+    private String shopOwner; // "Steve Jobs"
+
     private String email; // "steve@apple.com"
-    private String customer_email; // "customers@apple.com"
+
+    @JsonProperty("customer_email")
+    private String customerEmail; // "customers@apple.com"
+
     private String domain; // "shop.apple.com"
-    private String myshopify_domain; // "apple.myshopify.com"
+
+    @JsonProperty("myshopify_domain")
+    private String myshopifyDomain; // "apple.myshopify.com"
 
     private String address1; // "1 Infinite Loop"
     private String address2; // "Suite 100" private String city; // "Cupertino"
     private String city; // Cupertino
     private String province; // "California"
-    private String province_code; // "CA"
+
+    @JsonProperty("province_code")
+    private String provinceCode; // "CA"
+
     private String country; // "US"
-    private String country_code; // "US"
-    private String country_name; // "United States"
+
+    @JsonProperty("country_code")
+    private String countryCode; // "US"
+
+    @JsonProperty("country_name")
+    private String countryName; // "United States"
+
     private String phone; // "1231231234"
 
-    private String primary_locale; // "en"
+    @JsonProperty("primary_locale")
+    private String primaryLocale; // "en"
+
     private String currency; // "USD"
     private String timezone; // "(GMT-05:00) Eastern Time (US & Canada)"
 }
