5cb1121b00a544ef3bd08c513c8fd42a603a3e13
Tong Chen <tong@oath.com>
2018-01-23 16:41:22 -0800

    introduced child components

---
+++ ui/src/app/components/campaign/campaign.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CampaignChartComponent } from '../campaign-chart/campaign-chart.component';
+import { CampaignConfigComponent } from '../campaign-config/campaign-config.component';
+
+@Component({
+  selector: 'app-campaign',
+  entryComponents: [CampaignChartComponent, CampaignConfigComponent],
+  templateUrl: './campaign.component.html',
+  styleUrls: ['./campaign.component.css'],
+})
+export class CampaignComponent implements OnInit {
+  cmpId: number;
+  advId: number;
+  app_loaded = false;
+
+  constructor(private router: Router) {
+  }
+
+  static getQueryIntParam(reg): number {
+    const query = window.location.search;
+    const val = reg.exec(query);
+    return (val && val.length >= 1) ? Number.parseInt(val[1]) : null;
+  }
+
+  ngOnInit() {
+    this.cmpId = this.getCampaignIdParam();
+    this.advId = this.getAdvertiserIdParam();
+    this.app_loaded = true;
+
+    if (!(this.cmpId && this.advId)) {
+      this.router.navigateByUrl('login');
+    }
+  }
+
+  private getAdvertiserIdParam(): number {
+    const advertiserId = CampaignComponent.getQueryIntParam(/[?/&;,]?adv=([0-9]+)[&;,]?.*$/);
+    if (advertiserId) {
+      return advertiserId;
+    }
+    return 1643580; // TODO
+  }
+
+  private getCampaignIdParam(): number {
+    const campaignId = CampaignComponent.getQueryIntParam(/[?/&;,]?cmp=([0-9]+)[&;,]?.*$/);
+    if (campaignId) {
+      return campaignId;
+    }
+    return 363491351; // TODO;
+  }
+}
---
+++ ui/src/app/model/account.ts
@@ -0,0 +1,8 @@
+export class Account {
+  store_access_token: string;
+  store_native_acct_id: string;
+
+  yahoo_access_token: string;
+  gemini_native_acct_id: number;
+  campaign_id?: number;
+}
