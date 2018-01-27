00a94028b26933c0fbb1c77154b7c60588d5ac64
Tong Chen <tong@oath.com>
2018-01-26 18:44:13 -0800

    support popup window

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,19 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { CampaignService } from './services/campaign.service';
+import { MessageService } from './services/message.service';
 import { AppComponent } from './app.component';
 import { CampaignChartComponent } from './components/campaign-chart/campaign-chart.component';
 import { CampaignConfigComponent } from './components/campaign-config/campaign-config.component';
 import { CampaignComponent } from './components/campaign/campaign.component';
 import { LoginComponent } from './components/login/login.component';
 import { ShopifyComponent } from './components/shopify/shopify.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
 
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   declarations: [
     AppComponent,
     CampaignChartComponent,
@@ -21,11 +29,17 @@ import { ShopifyComponent } from './components/shopify/shopify.component';
     CampaignComponent,
     LoginComponent,
     ShopifyComponent,
+    PopupComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDialogModule,
+
     RouterModule.forRoot([
       {
         path: 'campaign',
@@ -40,6 +54,10 @@ import { ShopifyComponent } from './components/shopify/shopify.component';
         component: ShopifyComponent,
       },
       {
+        path: 'g/shopify/ews',
+        component: ShopifyComponent,
+      },
+      {
         path: '',
         redirectTo: 'campaign',
         pathMatch: 'full'
@@ -47,12 +65,16 @@ import { ShopifyComponent } from './components/shopify/shopify.component';
     ], {enableTracing: false})
   ],
   exports: [
-    RouterModule
+    RouterModule,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   providers: [
-    CampaignService
+    CampaignService,
+    MessageService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [PopupComponent]
 })
 export class AppModule {
 }
