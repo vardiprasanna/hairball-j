341c57b6eab2f4e165c81629c1298f35f4a84739
Tong Chen <tong@oath.com>
2018-01-30 21:01:59 -0800

    environment settings

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   ewsBaseUrl: '',
-
-
+  geminiHomeUrl: 'https://gemini.yahoo.com/advertiser/home',
+  geminiSigInMessage: 'You will be redirected to Yahoo\'s login page, and then automatically brought back here once you\'re done.',
+  geminiSigUpMessage: 'You will be redirected to Yahoo Gemini page to create a new account. Make sure that you complete your billing info as well because otherwise your product ads will not run.'
 };
