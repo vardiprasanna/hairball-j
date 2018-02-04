5c7f2c0919d109f35e1f846128a28622c9babd41
Tong Chen <tong@oath.com>
2018-02-04 13:30:33 -0800

    config for local dev

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   '&client_id=dj0yJmk9NEJVRHRaRnpWa09SJmQ9WVdrOVREQktiREUzTjJrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1iYQ--' +
   '&redirect_uri=https%3A%2F%2Fhairball.herokuapp.com%2Findex.html%3Froute%3Df%2Fshopify%2Fews',
 
-  ewsBaseUrl: '',
+  ewsBaseUrl: 'https://localhost:4443',
   geminiHomeUrl: 'https://gemini.yahoo.com/advertiser/home',
   geminiSigInMessage: 'You will be redirected to Yahoo\'s login page, and then automatically brought back here once you\'re done.',
   geminiSigUpMessage: 'You will be redirected to Yahoo Gemini page to create a new account. Make sure that you complete your billing info as well because otherwise your product ads will not run.'
