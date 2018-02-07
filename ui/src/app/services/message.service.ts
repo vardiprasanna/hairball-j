c6ea1cccb31778bfc52a0ce297a2359d2dd20896
Tong Chen <tong@oath.com>
2018-02-07 07:02:39 -0800

    added an alert-type of a message

--- ui/src/app/services/message.service.ts
+++ ui/src/app/services/message.service.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { PopupComponent, MessageConfig } from '../components/popup/popup.component';
 
+export interface Message {
+  text: string;
+  cssType?: string;
+}
+
 @Injectable()
 export class MessageService {
   dialogRef: any;
-  messages: string[];
+  messages: Message[];
 
   constructor(private dialog: MatDialog) {
   }
@@ -14,14 +19,20 @@ export class MessageService {
     return !this.messages || this.messages.length === 0;
   }
 
-  push(msg: string): void {
+  push(msg: string, type?: string): void {
     if (!this.messages) {
       this.messages = [];
     }
-    this.messages.push(msg);
+    if (!type) {
+      type = 'warning';
+    }
+    this.messages.push({
+      text: msg,
+      cssType: type
+    });
   }
 
-  pop(): string {
+  pop(): Message {
     return this.isEmpty() ? null : this.messages.pop();
   }
 
@@ -31,7 +42,10 @@ export class MessageService {
    */
   show(msg?: string, buttons?: any[], title?: string, alertType?: string): MatDialogRef<PopupComponent> {
     if (!msg) {
-      msg = this.pop();
+      const queuedMsg = this.pop();
+      if (queuedMsg) {
+        msg = queuedMsg.text;
+      }
     }
     if (msg) {
       const messageConfig: MessageConfig = {
