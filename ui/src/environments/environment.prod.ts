8373ab752501c0e66feff11b5bfb130a850eb516
Tong Chen <tong@oath.com>
2018-02-19 16:26:54 -0800

    tidy up the format

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -14,7 +14,8 @@ export const environment = {
   geminiMinBidBudget: 'Minimum budget is $5.00',
   geminiHomeUrl: 'https://gemini.yahoo.com/advertiser/home',
   geminiSigInMessage: 'You will be redirected to Yahoo\'s login page, and then automatically brought back here once you\'re done.',
-  geminiSigUpMessage: 'You will be redirected to Yahoo Gemini page to create a new account. Make sure that you complete your billing info as well because otherwise your product ads will not be served.',
+  geminiSigUpMessage: `You will be redirected to Yahoo Gemini page to create a new account.
+                          Make sure that you complete your billing info as well because otherwise your product ads will not be served.`,
   geminiUpdateSuccessful: 'Successfully updated.',
   geminiStartSuccessful: 'Successfully started.',
   geminiStopSuccessful: 'Successfully stopped.'
