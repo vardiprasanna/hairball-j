242ff71cd06b5f5591172b38a8a635b5ff4ec4a8
Prasannakumar Vardi <pvardi@yahoo-inc.com>
2018-03-02 16:35:32 -0800

    add to users to application properties

--- src/main/java/com/oath/gemini/merchant/security/YBYCookieValidator.java
+++ src/main/java/com/oath/gemini/merchant/security/YBYCookieValidator.java
@@ -5,11 +5,15 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
+import java.util.GregorianCalendar;
 import java.util.Map;
 import javax.ws.rs.core.Cookie;
+
+import com.oath.gemini.merchant.AppConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import com.yahoo.bouncer.sso.CookieInfo;
 import com.yahoo.bouncer.sso.CookieValidator;
+import org.apache.commons.configuration.Configuration;
 
 /**
  * @author tong on 10/1/2017
@@ -17,6 +21,13 @@ import com.yahoo.bouncer.sso.CookieValidator;
 @Slf4j
 public class YBYCookieValidator {
     private static CookieValidator cookieValidator = new CookieValidator();
+    private static String AppId;
+    private static String userArray;
+    static {
+        Configuration config = AppConfiguration.getConfig();
+        AppId = config.getString("app.id");
+        userArray = config.getString("app.users");
+    }
 
     static boolean init() {
         try {
@@ -35,8 +46,9 @@ public class YBYCookieValidator {
             final CookieInfo cookieInfo = cookieValidator.authSig(cookie.getValue());
             if (cookieInfo != null) {
                 String[] userRoles = cookieInfo.getUserRoles();
-                boolean contains = Arrays.asList(userRoles).contains("121.U");
-                return (contains && cookieInfo.getUserId().equals("pvardi"));
+                boolean contains = Arrays.asList(userRoles).contains(AppId);
+                String[] users = userArray.split(",");
+                return (contains && Arrays.asList(users).contains(cookieInfo.getUserId()));
             }
         }
         return false;
